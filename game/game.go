46c2979030b9978904a216b51cf179c6089ab00e
Vicente Reyes <vicente@vichoreyes.cl>
2025-07-24 22:19:04 -0400

    wip: running main game binary

--- game/game.go
+++ game/game.go
@@ -2,7 +2,11 @@ package game
 
 import (
 	"el_poblador/board"
+	"fmt"
 	"math/rand/v2"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Player struct {
@@ -25,6 +29,7 @@ const (
 	BeforeDice TurnPhase = iota
 	Robbing
 	TurnMain
+	OfferingTrade
 )
 
 // Meaningful zero value
@@ -35,6 +40,47 @@ type Game struct {
 	gamePhase  GamePhase
 	turnPhase  TurnPhase
 	lastDice   [2]int
+}
+
+func (g *Game) Print(width, height int) string {
+	margin := lipgloss.NewStyle().Margin(1)
+	// Title text styled with bold.
+	titleText := lipgloss.NewStyle().Bold(true).Render("El Poblador - Bubble Tea Version")
+
+	// The title is placed in the center of the screen, with a margin below it.
+	renderedTitle := lipgloss.PlaceHorizontal(width, lipgloss.Center, titleText)
+
+	// Help text styled as faint.
+	helpText := lipgloss.NewStyle().Faint(true).Render("Press 'c' or 'l' to regenerate the board. Press 'q' to quit.")
+
+	// The help text is placed in the center of the screen.
+	renderedHelp := lipgloss.PlaceHorizontal(width, lipgloss.Center, helpText)
+
+	// Board
+	boardLines := g.board.Print()
+	boardContent := strings.Join(boardLines, "\n")
+
+	// players
+	playerNames := make([]string, len(g.players))
+	for i, player := range g.players {
+		playerNames[i] = fmt.Sprintf("\033[38;5;%dm %s \033[0m", player.color, player.Name)
+	}
+	players := strings.Join(playerNames, "\n\n")
+	dice := "⚂ ⚄"
+	sidebar := margin.Render(lipgloss.JoinVertical(lipgloss.Left, dice, players))
+	renderedPlayers := lipgloss.JoinHorizontal(lipgloss.Top, boardContent, sidebar)
+
+	// Calculate the available space for the board.
+	availableHeight := height - lipgloss.Height(renderedTitle) - lipgloss.Height(renderedHelp)
+
+	// Main content is the board, centered in the available space.
+	mainContent := lipgloss.Place(width, availableHeight,
+		lipgloss.Center,
+		lipgloss.Center,
+		renderedPlayers,
+	)
+
+	return lipgloss.JoinVertical(lipgloss.Left, renderedTitle, mainContent, renderedHelp)
 }
 
 func (g *Game) Start(playerNames []string) {
--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
+	"el_poblador/game"
 	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main() {
-	fmt.Println("Settlers of Catan - TUI Version")
+type model struct {
+	game   game.Game
+	width  int
+	height int
+}
 
-	// TODO: run game here
-	fmt.Println("TODO: Implement game logic")
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return m.game.Print(m.width, m.height)
+}
+
+func main() {
+	game := game.Game{}
+	if len(os.Args) < 4 || len(os.Args) > 5 {
+		fmt.Println("Please provide 3-4 player names as arguments")
+		os.Exit(1)
+	}
+	game.Start(os.Args[1:])
+	p := tea.NewProgram(model{game: game})
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
